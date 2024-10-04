5b7439604f314c16a6430289543d39b3f285c139
CristianoSantan <cristiano.mer@gmail.com>
2024-10-04 17:17:30 -0300

    3 - ao clicar no botão. Devem ser exibidos o nome (name), o clima (climate), a população (population) e o tipo de terreno (terrain).

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let apiStarWars = 'https://swapi.dev/api/planets/?format=json';
 let planets = document.getElementById('planets');
+let info = document.getElementById('info');
 
 async function printPlanets() {
   let {results} = await fetchApi(apiStarWars);
@@ -7,6 +8,7 @@ async function printPlanets() {
   results.forEach(planet => {
     let button = document.createElement('button');
     button.textContent = planet.name;
+    button.onclick = () => infoPlanet(planet);
     planets.appendChild(button);
   });
 };
@@ -14,6 +16,21 @@ async function printPlanets() {
 async function fetchApi(url) {
   let res = await fetch(url);
   return await res.json();
+};
+
+async function infoPlanet(planet) {
+  let div = document.createElement('div');
+  info.innerHTML = '';
+  div.innerHTML = `
+    <ul>
+      <li><b>Planeta:</b> ${planet.name}</li>
+      <li><b>Clima:</b>  ${planet.climate}</li>
+      <li><b>População:</b>  ${planet.population}</li>
+      <li><b>Terreno:</b>  ${planet.terrain}</li>
+    </ul>
+  `;
+
+  info.appendChild(div);
 };
 
 printPlanets();
