45fc1c6a33a2843e2da88b2097819186f3deb4bf
CristianoSantan <cristiano.mer@gmail.com>
2024-10-04 16:59:41 -0300

    2 - Para cada planeta obtido na API no passo anterior, crie um botão na tela.

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 let apiStarWars = 'https://swapi.dev/api/planets/?format=json';
+let planets = document.getElementById('planets');
 
 async function printPlanets() {
   let {results} = await fetchApi(apiStarWars);
   
-  results.forEach(planet => console.log(planet.name));
+  results.forEach(planet => {
+    let button = document.createElement('button');
+    button.textContent = planet.name;
+    planets.appendChild(button);
+  });
 };
 
 async function fetchApi(url) {
